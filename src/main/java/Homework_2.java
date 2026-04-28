f25900068f5e28400457c2abbfafa980daff52dd
yanavzhn <yana.vezhnovec@gmail.com>
2026-04-28 12:31:54 +0300

    Домашняя работа 2

--- src/main/java/Homework_2.java
+++ src/main/java/Homework_2.java
@@ -5,16 +5,20 @@ public class Homework_2 {
         Random rnd = new Random();
 
         int randomAgeVisitor = rnd.nextInt(10, 100);
+        System.out.println("Возраст посетителя: " + randomAgeVisitor);
+
         double randomMoneyVisitor = rnd.nextDouble(10000, 100000);
+        System.out.printf("Денег у посетителя: %.2f%n", randomMoneyVisitor);
+
         boolean hasInvite = false;
         boolean isBlackList = false;
 
-        boolean isAdult = randomAgeVisitor >=18;
+        boolean isAdult = randomAgeVisitor >= 18;
         boolean hasInviteMoney = hasInvite || randomMoneyVisitor > 50000;
         boolean isClean = !isBlackList;
         boolean isAllowed = isAdult && hasInviteMoney && isClean;
 
-        System.out.println(isAllowed);
+        System.out.println("Допуск  на закрытую встречу: " + isAllowed);
 
         double contribution = randomMoneyVisitor * 0.075;
         System.out.printf("Добровольный взнос: %.2f%n", contribution);
